8779f68f7e7ba3dccba717d67d9263811b0afc10
MohamedEssamov <www.mohamedessam22@gmail.com>
2023-06-16 16:42:57 -0700

    qerq

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,21 +4,21 @@
 
 /**
  *	* main - prints the last digit of the random
- *	* the number is stored in the variable a
+ *	* the number is stored in the variable n
  *	* Return: Always 0 (Success)
  **/
 int main(void)
 {
-		int a;
+		int n;
 
 			srand(time(0));
-			a = rand() - RAND_MAX / 2;
-			printf("Last digit of %d is %d ", a, a % 10);
+			n = rand() - RAND_MAX / 2;
+			printf("Last digit of %d is %d ", n, n % 10);
 			if (a % 10 > 5)
-				printf("and is greater than 5\a");
-			else if (a % 10 == 0)
-				printf("and is 0\a");
-			else if (a % 10 < 6 && a % 10 != 0)
-				printf("and is less than 6 and not 0\a");
+				printf("and is greater than 5\n");
+			else if (n % 10 == 0)
+				printf("and is 0\n");
+			else if (n % 10 < 6 && n % 10 != 0)
+				printf("and is less than 6 and not 0\n");
 			return (0);
 }
