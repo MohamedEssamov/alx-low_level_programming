bfd87509496272dca2428590a28b5883e708b499
MohamedEssamov <www.mohamedessam22@gmail.com>
2023-06-16 14:58:21 -0700

    qd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+/**
+ *  *main- main fun
+ *   *
+ *    *Return: well return
+ *     */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
