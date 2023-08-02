150091dd498bf5c148bdf34bbb18f22a78bc948f
MohamedEssamov <www.mohamedessam22@gmail.com>
2023-08-02 14:25:51 -0700

    this is an edit for the item

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,14 @@
 /**
  * _puts_recursion - function that prints a string.
  * @s: input
- * Return: always 0.
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_putchar(s + 1);
+		_putchar('\n')
+		return;
 	}
-	else
-		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
