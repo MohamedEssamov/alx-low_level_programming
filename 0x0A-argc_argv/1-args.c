56a6993c92f9a7d40f925793b8c0bd3b0f60de58
MohamedEssamov <www.mohamedessam22@gmail.com>
2023-08-04 16:19:52 -0700

    messa

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[] __attribute__((unsend)))
+int main(int argc, char *argv[])
 {
-
+	(void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
 	return (0);
 }
