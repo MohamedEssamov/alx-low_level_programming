58dcf3588c7b022c994d223e197adc02ca82bf56
MohamedEssamov <www.mohamedessam22@gmail.com>
2023-08-14 14:12:14 -0700

    message

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ * defining the name of the file
+ * to be compiled
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
